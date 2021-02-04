beefc59deb32da8e40a8fa67dc62bfd4e17e7e13
Muhammed Eyup <mohammed.ay99@gmail.com>
2021-02-04 20:36:16 +0300

    overflow fixed

--- src/components/repoCard.js
+++ src/components/repoCard.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { Button, Card } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "../Assets/card.css";
 
 const RepoCard = (props) => {
     return (
         <Card style={{ margin: 15, width: "20%", height: "30vh" }}>
-            <Card.Body>
-                <Card.Img src="https://picsum.photos/200/300" />
+            <Card.Body className="Cbody">
                 <Card.Title>{props.name}</Card.Title>
-                <Card.Text>{props.description}</Card.Text>
+                <Card.Text className="Ctext">{props.description}</Card.Text>
             </Card.Body>
             <Button href={props.repo_url} target="_blank">
                 Go to Repo
